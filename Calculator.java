58caf02ada75e5fb55a89ecf9720da3e11829122
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-04-03 23:35:04 -0400

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+  /* DO NOT CHANGE THE METHOD SIGNATURE */
+	public static double eval(String expression) {
+    return 0.0
+	}
+
+  /* DO NOT CHANGE THE METHOD SIGNATURE */
+	public static String infixToPostfix(String infix) {
+		return "";
+	}
+
+}
